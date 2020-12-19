78a6d003c6ca6cfec37d360e66af2d97635883c3
Alexandru Ghiura <ghalex@gmail.com>
2020-12-19 19:53:58 +0200

    Update logo

---
+++ src/components/index.js
@@ -0,0 +1,3 @@
+export { default as Logo } from "./Logo/index.vue";
+export { default as Sidebar } from "./Sidebar/index.vue";
+export { default as LayoutHeader } from "./LayoutHeader/index.vue";
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -2,23 +2,20 @@
   <Layout>
     <div class="pt-8 md:pt-16">
       <div class="flex flex-col items-center">
-        <div class="flex flex-col items-center mb-2 text-ui-primary">
-          <Logo :width="80" />
-          <h2 class="text-3xl text-6xl font-black tracking-tighter border-none">
-            DOCC
-          </h2>
-        </div>
+        <Logo :width="120" />
         <h1 class="text-4xl text-center lg:text-5xl">
-          Great Documentation starts here.
+          Vue3-Charts
         </h1>
         <p class="text-xl font-medium text-center">
-          Perfect Lighthouse score out of the box. Easy to set-up. Dark mode included.
-          <br>
+          ⚡ Easy and beautiful charts for Vue 3
+          <br />
           <strong class="text-ui-primary">Got a minute?</strong>
         </p>
 
         <div class="py-4">
-          <code class="block px-4 py-1 select-all bg-ui-border text-ui-typo">gridsome create your-project https://github.com/mrcrmn/docc</code>
+          <code class="block px-4 py-1 select-all bg-ui-border text-ui-typo">
+            npm i <b>vue3-charts</b>
+          </code>
         </div>
 
         <div class="flex justify-center mt-8">
@@ -32,52 +29,25 @@
         </div>
 
         <p class="mt-8 text-center">
-          Open Source. MIT License. <span class="border-b border-dashed border-ui-primary text-ui-primary">Make it your own</span>.
+          Open Source. MIT License.
+          <span class="border-b border-dashed border-ui-primary text-ui-primary"
+            >Make it your own</span
+          >.
         </p>
-
       </div>
-
-      <div class="pt-8 mx-auto mt-8 border-t md:mt-16 md:pt-16 border-top border-ui-border max-w-screen-sm"></div>
-
-      <div class="flex flex-wrap justify-center -mx-4">
-        <div class="flex flex-col items-center w-full px-4 mb-8 text-center md:w-1/3">
-          <ZapIcon size="3x" class="mb-6 text-ui-primary" />
-          <h3 class="font-bold tracking-wide uppercase text-ui-primary">
-            Incredibly Fast
-          </h3>
-          <p class="text-lg text-left">
-            Powered by Gridsome. Built on Vue. Outputs static files. It wont't get any faster than this. <span class="border-b border-dashed border-ui-primary text-ui-primary">No seriously</span>.
-          </p>
-        </div>
-
-        <div class="flex flex-col items-center w-full px-4 mb-8 text-center md:w-1/3">
-          <SearchIcon size="3x" class="mb-6 text-ui-primary" />
-          <h3 class="font-bold tracking-wide uppercase text-ui-primary">
-            Instant Search
-          </h3>
-          <p class="text-lg text-left">
-            All headlines are indexed. Instant search powered by Fuse.js. Find what you are looking for in the <span class="border-b border-dashed border-ui-primary text-ui-primary">blink of an eye</span>.
-          </p>
-        </div>
-
-        <div class="flex flex-col items-center w-full px-4 mb-8 text-center md:w-1/3">
-          <MoonIcon size="3x" class="mb-6 text-ui-primary" />
-          <h3 class="font-bold tracking-wide uppercase text-ui-primary">
-            Lights out
-          </h3>
-          <p class="text-lg text-left">
-            Working late at night again? Enable dark mode with the click of a button.
-          </p>
-        </div>
-      </div>
-
     </div>
   </Layout>
 </template>
 
 <script>
-import Logo from '@/components/Logo';
-import { ArrowRightCircleIcon, ZapIcon, CodeIcon, MoonIcon, SearchIcon } from 'vue-feather-icons';
+import { Logo } from "@/components";
+import {
+  ArrowRightCircleIcon,
+  ZapIcon,
+  CodeIcon,
+  MoonIcon,
+  SearchIcon,
+} from "vue-feather-icons";
 
 export default {
   components: {
@@ -86,44 +56,45 @@ export default {
     ZapIcon,
     CodeIcon,
     MoonIcon,
-    SearchIcon
+    SearchIcon,
   },
 
   metaInfo() {
-    const title = 'Great Documentation starts here';
-    const description = 'DOCC is a starter theme with instant search and dark mode for writing great technical documentation. Based on Gridsome!';
+    const title = "Great Documentation starts here";
+    const description =
+      "DOCC is a starter theme with instant search and dark mode for writing great technical documentation. Based on Gridsome!";
 
     return {
       title: title,
       meta: [
         {
-          name: 'description',
-          content: description
-        },
-        {
-          key: 'og:title',
-          name: 'og:title',
-          content: title,
-        },
-        {
-          key: 'twitter:title',
-          name: 'twitter:title',
-          content: title,
-        },
-        {
-          key: 'og:description',
-          name: 'og:description',
+          name: "description",
           content: description,
         },
         {
-          key: 'twitter:description',
-          name: 'twitter:description',
+          key: "og:title",
+          name: "og:title",
+          content: title,
+        },
+        {
+          key: "twitter:title",
+          name: "twitter:title",
+          content: title,
+        },
+        {
+          key: "og:description",
+          name: "og:description",
           content: description,
         },
-      ]
-    }
-  }
-}
+        {
+          key: "twitter:description",
+          name: "twitter:description",
+          content: description,
+        },
+      ],
+    };
+  },
+};
 </script>
 
 <style>
